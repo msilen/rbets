31cd0b28e9847fb462501a2d3f4f72073605d4f0
Martin Silenus <sileenus@gmail.com>
2011-06-22 14:34:17 +0900

    first test edit:patched to pass test

--- lib/rbets.rb
+++ lib/rbets.rb
@@ -3,12 +3,23 @@ require 'celerity'
 
 class EventsGetter
   SITE_LIST=['https://www.gamebookers.com/']
+
   def initialize
   end
 
   def run
-    browser=Celerity::Browser.new
+    establish_connection
+    get_list_of_links
+  end
+
+  def establish_connection
+    @browser=Celerity::Browser.new
     puts "receiving data..."
-    browser.goto(SITE_LIST[0])
+    @browser.goto(SITE_LIST[0])
+  end
+
+  def get_list_of_links
+    links= @browser.div(:class, "leftNavText").links
+    puts links
   end
 end
